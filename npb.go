eebe88f9d10415ba53788e8816f48c35af3b6469
vn2483 <vn2483@att.com>
2021-11-22 08:09:13 -0500

    Add power fetching

--- npb.go
+++ npb.go
@@ -41,11 +41,68 @@ func (c *Client) CloseSSH(session *ssh.Session) {
 
 // Show NPB Version
 func (c *Client) ShowVersion(session *ssh.Session) (VersionInfo, error) {
-	var readBytes bytes.Buffer
-	session.Stdout = &readBytes
-	session.Run("show version | json")
 	var version VersionInfo
+	var readBytes bytes.Buffer
+
+	session.Stdout = &readBytes
+	err := session.Run("show version | json")
+	if err != nil {
+		return version, err
+	}
+
 	//json.Unmarshal([]byte(readBytes.String()), &version)
 	json.Unmarshal(readBytes.Bytes(), &version)
 	return version, nil
+}
+
+// Show NPB Host Info
+func (c *Client) ShowHostname(session *ssh.Session) (HostInfo, error) {
+	var returnData HostInfo
+	var readBytes bytes.Buffer
+
+	session.Stdout = &readBytes
+	err := session.Run("show hostname | json")
+	if err != nil {
+		return returnData, err
+	}
+
+	json.Unmarshal(readBytes.Bytes(), &returnData)
+	return returnData, nil
+}
+
+// Show NPB Power Info
+func (c *Client) ShowPower(session *ssh.Session) (PowerInfo, error) {
+	var rawData RawPowerInfo
+	var returnData PowerInfo
+	var readBytes bytes.Buffer
+
+	session.Stdout = &readBytes
+	err := session.Run("show environment power | json")
+	if err != nil {
+		return returnData, err
+	}
+
+	json.Unmarshal(readBytes.Bytes(), &rawData)
+
+	returnData = PowerInfo{
+		PS1: rawData.PowerSupplies.Instance1.State,
+		PS2: rawData.PowerSupplies.Instance2.State,
+	}
+
+	return returnData, nil
+}
+
+// Show NPB NTP Info
+func (c *Client) ShowNtp(session *ssh.Session) (NtpInfo, error) {
+	var returnData NtpInfo
+	var readBytes bytes.Buffer
+
+	session.Stdout = &readBytes
+	err := session.Run("show ntp status | json")
+	if err != nil {
+		return returnData, err
+	}
+
+	json.Unmarshal(readBytes.Bytes(), &returnData)
+	return returnData, nil
 }
--- types.go
+++ types.go
@@ -20,3 +20,36 @@ type VersionInfo struct {
 	HwMacAddress       string  `json:"hwMacAddress"`
 	Architecture       string  `json:"architecture"`
 }
+
+//HostInfo
+type HostInfo struct {
+	Fqdn     string `json:"fqdn"`
+	HostName string `json:"hostname"`
+}
+
+//NtpInfo
+type NtpInfo struct {
+	Status            string `json:"status"`
+	PollingInterfal   int    `json:"pollingInterval"`
+	Stratum           int    `json:"stratum"`
+	MaxEstimatedError int    `json:"maxEstimatedError"`
+	Server            string `json:"server"`
+}
+
+//RawPowerInfo
+type RawPowerInfo struct {
+	PowerSupplies struct {
+		Instance1 struct {
+			State string `json:"state"`
+		} `json:"1"`
+		Instance2 struct {
+			State string `json:"state"`
+		} `json:"2"`
+	} `json:"powerSupplies"`
+}
+
+//PowerInfo
+type PowerInfo struct {
+	PS1 string
+	PS2 string
+}
